1b8846cbd61929487d0decbecf2eb43f07c44b55
Anna_Paraniak <Anuszka@Szarlota.local>
2017-09-24 16:03:25 +0200

    first commit

---
+++ src/app/input/input.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'todo-input',
+  template: `
+  <h1>
+  {{title}}
+</h1>
+<input [value]="generateNewTitle()"
+  #Anna>
+<button (click)="changeTitle(Anna.value)">Save</button>
+<p>The title is: {{ title }}</p>
+<ol>
+<li *ngFor="let item of todoList">
+{{item.title}}
+</li>
+</ol>
+`,
+  styleUrls: ['./input.component.css']
+})
+export class InputComponent implements OnInit {
+title = 'Hello';
+private todoList = [
+  {title: 'install NodeJS'},
+  {title: 'install Angular CLI'},
+  {title: 'create new app'},
+  {title: 'serve app'},
+  {title: 'develop app'},
+  {title: 'deploy app'},
+];
+  constructor() {
+    this.changeTitle('Angular is awesome');
+    console.log('after changing the title');
+}
+changeTitle(newTitle: string): void {
+  this.title = newTitle;
+}
+
+  ngOnInit() {
+  }
+generateNewTitle(): string {
+return 'This is awesome';
+}
+}
